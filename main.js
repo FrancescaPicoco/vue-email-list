b798d69552d42c380526da92df4455f90530cb2b
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-20 19:33:35 +0100

    errore

--- main.js
+++ main.js
@@ -2,36 +2,33 @@ const { createApp } = Vue
 const emails = {
     data(){
         return{
-            mailsArray : []
+            mailsArray :[],
         }
     },
     methods: {
         generatoreEmail() {
-            for (let i = 0; i < 10 + 1; i++) {
-                const indexEmail =i;
-                console.log(indexEmail)
+            for (let i = 0; i < 10 ; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
-                console.log(resp.data.response)
-                //this.mailsArray.push(resp.data.response)
+                console.log(resp.data.response)               
+                let singleEmail = resp.data.response;
+				this.mailsArray.push(singleEmail);
             });
-            }
-            
-        }
+            }           
+        }       
     },
     mounted() {
+        
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
             // resp.data = dati ottenuti in risposta dal server
             // resp.status = codice HTTP di risposta (es. 200)
             // resp.statusText = codice HTTP di risposta (es. OK)
             console.log("Ricevuto: ", resp.data);
-
-            //se la risposta è ok
+             //se la risposta è ok
             if(resp.data.success) {
-                this.mailsArray = resp.data.response;
-            }
+                this.mailsArray = (resp.data.response);
+            }           
         });
         
     }
-
 }
 createApp(emails).mount('#app')
