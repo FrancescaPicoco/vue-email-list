9c227d96f82124a0f2fb8b020723341bdf6c5c1a
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-21 10:30:55 +0100

    correzione con l'array svuotato allinizio

--- main.js
+++ main.js
@@ -3,17 +3,21 @@ const emails = {
     data(){
         return{
             mailsArray :[],
+            mailsOk : false,
         }
     },
     methods: {
         generatoreEmail() {
             for (let i = 0; i < 10 ; i++) {
+                this.mailsArray = [],
+                this.mailsOk = false
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
                 console.log(resp.data.response)               
                 let singleEmail = resp.data.response;
 				this.mailsArray.push(singleEmail);
             });
-            }           
+            }
+            this.mailsOk = true           
         }       
     },
     mounted() {
